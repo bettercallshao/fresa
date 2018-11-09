16617dd1d930f7546ad78217729d62ba0231af6a
Shaoqing Tan <tim.tan@hydiant.com>
2018-11-09 00:08:14 -0500

    WIP

--- pcgen/pcgen.py
+++ pcgen/pcgen.py
@@ -2,81 +2,31 @@ import msgpack as msp
 import nanomsg as nng
 import urllib.request
 import sys
+import time
+import numpy as np
 
-def PackMap(pm):
-    pkr = msp.Packer(autoreset = False)
-    for key in pm:
-        pkr.pack(key)
-        pkr.pack(pm[key])
-    return pkr.bytes()
+def GenPointCloud(cnt):
+    pts = []
+    for theta in np.linspace(0, np.pi / 2, 10):
+        x = 10 * np.cos(theta + cnt / 30.0)
+        y = 10 * np.sin(theta + cnt / 30.0)
+        for z in np.linspace(0, 10, 10):
+            pts.append([x, y, z, int(z * 100), int(200 - z * 100), int(128)])
 
-def UnpackValue(pm, key, upk):
-    value = upk.unpack()
-    if pm[key] == value:
-        return False
-    else:
-        pm[key] = value
-        return True
-
-def Notify(pm, key, pub, log):
-    pub.send(PackMap({ key: pm[key] }))
-    log.Send('%d <= (%s)' % (key, str(pm[key])), 1)
+    return pts, cnt + 1
 
 def Serve(config):
-    petter = Petter(config, 'cacher')
-    log = LogSender(config, 'cacher')
-
-    # create param map
-    pm = {}
-    for p in config.Params:
-        pm[p['Key']] = None
-
-    sm = {}
-    for s in config.Series:
-        sm[s['Key']] = s['Serie']
-
-    rep = nng.Socket(nng.REP)
-    repurl = 'tcp://*:%d' % config.CacherCmdPort
-    rep.bind(repurl)
-
     pub = nng.Socket(nng.PUB)
-    puburl = 'tcp://*:%d' % config.CacherDatPort
+    puburl = 'ws://*:%d' % config['PcgenDatPort']
     pub.bind(puburl)
 
-    log.Send('Server started: ' + config.VersionStr +
-             ', REP: ' + repurl +
-             ', PUB: ' + puburl)
-
-    try:
-        while True:
-            # prepare a stream unpacker
-            upk = msp.Unpacker()
-            # wait for new msg
-            upk.feed(rep.recv())
-            # find key
-            key = upk.unpack()
-            if key == 0:
-                # key 0 is special
-                rep.send(PackMap(pm))
-            elif key < 0:
-                # series
-                rep.send('')
-                serie = sm[key]
-                for paramKey in serie:
-                    print(paramKey)
-                    if UnpackValue(pm, paramKey, upk):
-                        Notify(pm, paramKey, pub, log)
-            else:
-                # one param
-                rep.send('')
-                if UnpackValue(pm, key, upk):
-                    Notify(pm, key, pub, log)
-
-    except(KeyboardInterrupt):
-        pass
-
-    petter.Stop()
-    log.Send('Server stopped')
+    cnt = 0
+    while True:
+        time.sleep(1)
+        data, cnt = GenPointCloud(cnt)
+        pkr = msp.Packer(autoreset = False)
+        pkr.pack(data)
+        pub.send(pkr.bytes())
 
 def GetConfig(addr):
     return urllib.request.urlopen('http://' + addr + ':8999/fresa/config').read()
@@ -87,4 +37,5 @@ if __name__ == '__main__':
         print('Usage: ' + sys.argv[0] + ' <addr>')
         exit(-1)
 
-    c = GetConfig(sys.argv[1])
+    #c = GetConfig(sys.argv[1])
+    Serve({'PcgenDatPort': 9200})
